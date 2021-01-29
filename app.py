ead97eb6ac3ee83d75ff9d6164fa87e26ca1479e
bmai <bmai@sfu.ca>
2021-01-29 01:34:14 -0800

    working chat app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, render_template
 from flask_cors import CORS, cross_origin
 from bot.chat import init_bot, chat
 
@@ -8,16 +8,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 intents, all_words, tags, model, device = init_bot()
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
 
-@app.route('/chat', methods=['POST'])
+@app.route('/chat', methods=['GET', 'POST'])
 @cross_origin()
 def get_bot_response():
     sentence = request.json['sentence']
     response = chat(sentence, intents, all_words, tags, model, device)
-    return make_response(jsonify({"response" : response}), 400)
+    return make_response(jsonify({"response" : response}), 200)
+   
 
 @app.errorhandler(500)
 def server_error(e):
