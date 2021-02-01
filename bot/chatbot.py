18c4baf06ce3f804b2d1ebfc9220858075006f3f
bmai <bmai@sfu.ca>
2021-01-31 21:22:44 -0800

    refactor: created chatbot class

---
+++ bot/chatbot.py
@@ -0,0 +1,52 @@
+import random
+import json
+import os
+import torch
+from .nn_model import NeuralNet
+from .nltk_utils import bag_of_words, tokenize
+
+
+class ChatBot():
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    def __init__(self):
+        with open('intents.json', 'r') as f:
+            self.intents = json.load(f)
+
+        FILE = 'data.pth'
+        data = torch.load(os.path.join(os.path.dirname(__file__), '..', FILE))
+
+        self.input_size = data["input_size"]
+        self.hidden_size = data["hidden_size"]
+        self.output_size = data["output_size"]
+
+        self.all_words = data["all_words"]
+        self.tags = data["tags"]
+        self.model_state = data["model_state"]
+
+        self.model = NeuralNet(
+            self.input_size, self.hidden_size, self.output_size).to(ChatBot.device)
+        self.model.load_state_dict(self.model_state)  # load learned parameters
+        self.model.eval()
+
+    def chat(self, sentence):
+        sentence = tokenize(sentence)
+        X = bag_of_words(sentence, self.all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(ChatBot.device)
+
+        # get result from model
+        output = self.model(X)
+        _, predicted = torch.max(output, dim=1)
+        tag = self.tags[predicted.item()]
+
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+
+        if prob.item() > 0.75:
+            for intent in self.intents["intents"]:
+                if tag == intent["tag"]:
+                    return random.choice(intent['responses'])
+        else:
+            return "I do not understand ):"
---
+++ cli_chat.py
@@ -0,0 +1,13 @@
+from bot.chatbot import ChatBot
+
+if __name__ == '__main__':
+
+    bot_name = "BennyBot"
+    bot = ChatBot()
+    print(f"{bot_name}: Hi how are you! Type 'quit' to exit.")
+    while True:
+        sentence = input("You: ")
+        if sentence == 'quit':
+            break
+        response = bot.chat(sentence)
+        print(f"{bot_name}: {response}")
